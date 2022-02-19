5901fa15536b03538a8e2f2d990229fa791b6843
ViVidam <1325507519@qq.com>
2022-02-19 13:25:41 -0800

    add framework

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import json
 from header import *
 import time
+import threading
 
 class UDPSocket:
     buffersize = 1024
@@ -20,3 +21,24 @@ class UDPSocket:
         data, clientIP = self.UDPsocket.recvfrom(self.buffersize)
         data = json.loads(data.decode())
         return data, clientIP
+
+class PersistFile:
+    def __init__(self,id):
+        self.log = "log" +str(id)
+        self.keyPair = ("public"+str(id),"private"+str(id))
+
+class Client:
+    def __init__(self,id,mode):
+        self.id = id
+        self.socket = UDPSocket(id)
+        self.mode = mode
+        self.persistFile = PersistFile(id)
+
+    def broadcast(self,receiverGroupId,data):
+        threads = []
+        for receiverId in receiverGroupId:
+            threads.append(threading.Thread(target=self.socket.sendMessage, args=(data, clientIPs[receiverId])))
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
